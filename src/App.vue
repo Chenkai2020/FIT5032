7ea15b6e09d7290dc43f40abb6d123ccb216e296
Chenkai2020 <cdou0018@student.monash.edu>
2025-10-07 22:12:15 +1100

    week9-10

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,33 @@ import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
 </script>
 
 <template>
-  <header>
-    <BHeader />
-  </header>
-
-  <main>
-    <!-- <LibraryRegistrationForm /> -->
-    <!-- <JSONLab /> -->
-    <router-view></router-view>
-  </main>
+  <div class="main-container">
+    <header v-if="showHeader">
+      <BHeader />
+    </header>
+    <main class="main-box">
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
+
+<script>
+import BHeader from './components/BHeader.vue';
+import CountBookAPI from "./views/CountBookAPI.vue";
+
+export default {
+  name: 'App',
+  components: {
+    BHeader,
+    CountBookAPI
+  },
+  computed: {
+    showHeader() {
+      return this.$route.name !== 'CountBookAPI';
+    }
+  }
+};
+</script>
 
 <style scoped>
 /* header {
